3d35e24b4e93b6065b5d86fd50ff07c57ba24990
Tofik Mussa <tofahub@gmail.com>
2020-06-28 15:49:22 -0400

    setup spring boot

---
+++ server/LibStatsHelper/src/main/java/com/lib/stats/helper/LibStatsHelper/LibStatsHelperApplication.java
@@ -0,0 +1,13 @@
+package com.lib.stats.helper.LibStatsHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibStatsHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibStatsHelperApplication.class, args);
+	}
+
+}
