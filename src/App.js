25e89672c63e510416fcb68bb033f6bdafb2c4d5
RamOchoa-HES <ram.ochoa@yahoo.com>
2020-06-29 16:18:01 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import LandingPage from './components/LandingPage';
+import './bootstrap.css';
+//app "root" component imported into src/index.js
+ 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {/*embedded child components into app parent component*/}
+          <LandingPage />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+// renders app componenet into the root element of public/index.html
 ReactDOM.render(
   <React.StrictMode>
     <App />
