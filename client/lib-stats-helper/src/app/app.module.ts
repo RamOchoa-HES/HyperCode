c64215c4551a26242dd29c41652c957ef03d0d8c
Tofik Mussa <tofahub@gmail.com>
2020-07-03 22:04:49 -0400

    housekeeping

--- client/lib-stats-helper/src/app/app.module.ts
+++ client/lib-stats-helper/src/app/app.module.ts
@@ -28,6 +28,8 @@ import { AddNewTechnicalComponent } from './technical/add-new-technical/add-new-
 import { ViewPastTechnicalComponent } from './technical/view-past-technical/view-past-technical.component';
 import { AddNewElectronicComponent } from './electronic/add-new-electronic/add-new-electronic.component';
 import { ViewPastElectronicComponent } from './electronic/view-past-electronic/view-past-electronic.component';
+import { LogOutComponent } from './log-out/log-out.component';
+import { RegisterUserComponent } from './register-user/register-user.component';
 
 @NgModule({
   declarations: [
@@ -56,6 +58,8 @@ import { ViewPastElectronicComponent } from './electronic/view-past-electronic/v
     ViewPastTechnicalComponent,
     AddNewElectronicComponent,
     ViewPastElectronicComponent,
+    LogOutComponent,
+    RegisterUserComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
