d1054ce1064b9d36ded0010506fcad2e72342597
Tofik Mussa <tofahub@gmail.com>
2020-07-04 16:58:58 -0400

    technical services working

--- client/lib-stats-helper/src/app/models/models.ts
+++ client/lib-stats-helper/src/app/models/models.ts
@@ -50,6 +50,24 @@ export class ERCMarketing {
   patreonMonth4: number;
   patreonMonth4Name: string;
 }
+
+export class TechincalService {
+  year: number;
+  semester: String;
+  numberOfBooksDVDs: number;
+  additionalItems: number;
+  numberOfEbooks: number;
+  patreonMonth1: number;
+  patreonMonth1Name: string;
+  patreonMonth2: number;
+  patreonMonth2Name: string;
+  patreonMonth3: number;
+  patreonMonth3Name: string;
+  patreonMonth4: number;
+  patreonMonth4Name: string;
+  isCorrect: number;
+}
+
 export const Months = [
   "January",
   "February",
--- client/lib-stats-helper/src/app/technical/add-new-technical/add-new-technical.component.ts
+++ client/lib-stats-helper/src/app/technical/add-new-technical/add-new-technical.component.ts
@@ -1,15 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { TechincalService, Months } from "src/app/models/models";
 
 @Component({
-  selector: 'app-add-new-technical',
-  templateUrl: './add-new-technical.component.html',
-  styleUrls: ['./add-new-technical.component.css']
+  selector: "app-add-new-technical",
+  templateUrl: "./add-new-technical.component.html",
+  styleUrls: ["./add-new-technical.component.css"],
 })
 export class AddNewTechnicalComponent implements OnInit {
+  techincalService: TechincalService = new TechincalService();
 
-  constructor() { }
+  monthList = Months;
 
-  ngOnInit() {
+  selectedMonth1: any = null;
+  selectedMonth2: any = null;
+  selectedMonth3: any = null;
+  selectedMonth4: any = null;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  isInvalid() {
+    if (
+      !this.techincalService ||
+      !this.techincalService.year ||
+      !this.techincalService.semester ||
+      !this.techincalService.numberOfBooksDVDs ||
+      !this.techincalService.numberOfEbooks ||
+      !this.techincalService.additionalItems ||
+      !this.techincalService.isCorrect
+    ) {
+      return true;
+    }
+    return false;
   }
 
+  reset() {
+    this.techincalService.year = 0;
+    this.techincalService.semester = "";
+    this.techincalService.numberOfBooksDVDs = 0;
+    this.techincalService.numberOfEbooks = 0;
+    this.techincalService.additionalItems = 0;
+    this.techincalService.isCorrect;
+    this.techincalService.patreonMonth1 = 0;
+    this.techincalService.patreonMonth1Name = "";
+    this.techincalService.patreonMonth2 = 0;
+    this.techincalService.patreonMonth2Name = "";
+    this.techincalService.patreonMonth3 = 0;
+    this.techincalService.patreonMonth3Name = "";
+    this.techincalService.patreonMonth4 = 0;
+    this.techincalService.patreonMonth4Name = "";
+  }
+
+  addTechnicalService() {
+    console.log("technical ", this.techincalService);
+  }
+
+  selectedMonth1Func(value) {
+    this.selectedMonth1 = value;
+    this.techincalService.patreonMonth1Name = this.selectedMonth1;
+  }
+
+  selectedMonth2Func(value) {
+    this.selectedMonth2 = value;
+    this.techincalService.patreonMonth2Name = this.selectedMonth2;
+  }
+
+  selectedMonth3Func(value) {
+    this.selectedMonth3 = value;
+    this.techincalService.patreonMonth3Name = this.selectedMonth3;
+  }
+
+  selectedMonth4Func(value) {
+    this.selectedMonth4 = value;
+    this.techincalService.patreonMonth4Name = this.selectedMonth4;
+  }
 }
