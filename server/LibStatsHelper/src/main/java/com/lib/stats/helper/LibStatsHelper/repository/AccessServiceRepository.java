3eee7f7af564c97c970cb3f01c96a677ea629305
Tofik Mussa <tofahub@gmail.com>
2020-07-05 02:26:53 -0400

    changed repository method

--- server/LibStatsHelper/src/main/java/com/lib/stats/helper/LibStatsHelper/repository/AccessServiceRepository.java
+++ server/LibStatsHelper/src/main/java/com/lib/stats/helper/LibStatsHelper/repository/AccessServiceRepository.java
@@ -3,7 +3,12 @@ package com.lib.stats.helper.LibStatsHelper.repository;
 import com.lib.stats.helper.LibStatsHelper.entity.AccessService;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AccessServiceRepository extends JpaRepository<AccessService, Integer> {
-    AccessService findBySemester(String semester);
+
+    AccessService findAccessServiceBySemesterContaining(String semester);
+
+    List<AccessService> findAccessServiceByYearEquals(int year);
 }
 
