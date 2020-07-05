b947b56cbd453b0c293492e0c14fcf1cb8afef9f
Tofik Mussa <tofahub@gmail.com>
2020-07-05 13:16:50 -0400

    fixed controller bug

--- server/LibStatsHelper/src/main/java/com/lib/stats/helper/LibStatsHelper/controller/AccessServiceController.java
+++ server/LibStatsHelper/src/main/java/com/lib/stats/helper/LibStatsHelper/controller/AccessServiceController.java
@@ -5,17 +5,19 @@ import com.lib.stats.helper.LibStatsHelper.service.AccessServiceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController(value ="/api/access")
+@RestController
+@RequestMapping("/api/access")
 public class AccessServiceController {
 
     @Autowired
     private AccessServiceService accessServiceService;
 
-    @GetMapping(value = "/")
+    @GetMapping
     public ResponseEntity<List<AccessService>> getAllAccessServices(){
         return ResponseEntity.ok(accessServiceService.getAccessServices());
     }
