7115dc5ef51e542170aaa31a3973de7be52f03cb
Tofik Mussa <tofahub@gmail.com>
2020-07-05 13:23:39 -0400

    fixed merge conflict

--- server/LibStatsHelper/src/main/java/com/lib/stats/helper/LibStatsHelper/controller/AccessServiceController.java
+++ server/LibStatsHelper/src/main/java/com/lib/stats/helper/LibStatsHelper/controller/AccessServiceController.java
@@ -3,22 +3,17 @@ package com.lib.stats.helper.LibStatsHelper.controller;
 import com.lib.stats.helper.LibStatsHelper.entity.AccessService;
 import com.lib.stats.helper.LibStatsHelper.service.AccessServiceService;
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-=======
->>>>>>> 094caec6d36a9fdb9cbbe58131bbd2c57c7a933e
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-<<<<<<< HEAD
 @RequestMapping("/api/access")
-=======
->>>>>>> 094caec6d36a9fdb9cbbe58131bbd2c57c7a933e
 public class AccessServiceController {
     @Autowired
     private AccessServiceService service;
@@ -53,15 +48,14 @@ public class AccessServiceController {
 //        return service.updateAccessService(accessservice);
 //    }
 
-<<<<<<< HEAD
     @GetMapping
-    public ResponseEntity<List<AccessService>> getAllAccessServices(){
-        return ResponseEntity.ok(accessServiceService.getAccessServices());
-=======
+    public ResponseEntity<List<AccessService>> getAllAccessServices() {
+        return ResponseEntity.ok(service.getAccessServices());
+    }
+
     @DeleteMapping("/delete/{id}")
     public String deleteAccessService(@PathVariable int id) {
         return service.deleteAccessService(id);
->>>>>>> 094caec6d36a9fdb9cbbe58131bbd2c57c7a933e
     }
 
 }
