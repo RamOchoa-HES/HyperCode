f5db23a8be99a184e9e80ab82d5913d89894dfb7
Tofik Mussa <tofahub@gmail.com>
2020-07-08 16:04:30 -0400

    resolved router issue

--- client/lib-stats-helper/src/app/app-routing.module.ts
+++ client/lib-stats-helper/src/app/app-routing.module.ts
@@ -27,10 +27,9 @@ import { AddNewTechnicalComponent } from "./technical/add-new-technical/add-new-
 import { ViewPastTechnicalComponent } from "./technical/view-past-technical/view-past-technical.component";
 import { AddNewElectronicComponent } from "./electronic/add-new-electronic/add-new-electronic.component";
 import { ViewPastElectronicComponent } from "./electronic/view-past-electronic/view-past-electronic.component";
-import { LogOutComponent } from "./log-out/log-out.component";
 
 export const routes: Routes = [
-  { path: "", component: AppComponent, pathMatch: "full" },
+  { path: "", component: HomeComponent, pathMatch: "full" },
   { path: "home", component: HomeComponent },
   { path: "access/add", component: AddNewComponent },
   { path: "access/view", component: ViewPastComponent },
