2e4fd5c784bc65bbb35f054e7eacce0cb1f9f151
RamOchoa-HES <ram.ochoa@yahoo.com>
2020-07-08 21:21:48 -0500

    removed welcome message

--- client/lib-stats-helper/e2e/src/app.e2e-spec.ts
+++ client/lib-stats-helper/e2e/src/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display library name', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to lib-stats-helper!');
+    expect(page.getTitleText()).toEqual('Regina Library');
   });
 
   afterEach(async () => {
